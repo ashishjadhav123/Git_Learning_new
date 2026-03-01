f4930ab112cffb21bf7d8ddc750e144f1a1284d5
Ashish <jadhavashish028@gmail.com>
2026-03-01 18:58:45 +0530

    Merge conflict main

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def print_msg(msg):
 if __name__ == '__main__':
     # Adding feature 1 for process
     # Changes in main
-    # create merge conflict
+    # create merge conflict feature-2
     input_msg = input("Enter your message: ")
     print_msg(input_msg)
     print("Bye")
